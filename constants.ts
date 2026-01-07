4c53628077d3bba27e56256dcb8f3e663d025c36
aperry938 <anthonycperry21@gmail.com>
2026-01-07 05:21:31 -0500

    Increase breathing exercise icons to h-8 w-8

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@ import { Soundscape, GuidedRelaxation } from './types';
 // Standard props for consistent icon styling
 const iconProps = {
     xmlns: "http://www.w3.org/2000/svg",
-    className: "h-6 w-6",
+    className: "h-8 w-8",
     fill: "none",
     viewBox: "0 0 24 24",
     stroke: "currentColor",
